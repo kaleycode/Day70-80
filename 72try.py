cdc5cd66a3e8bfcb47b836ba1df7e6f237ad360c
kaleycode <readingkale@gmail.com>
2024-08-04 12:43:12 -0700

    72try.py

--- 72try.py
+++ 72try.py
@@ -1,22 +1,19 @@
 from replit import db
 import os, time, datetime, random
+
 def newUser():
-  print()
-  username = input("Username: ")
-  password = input("Password: ")
-  keys = db.keys()
-  if username in keys:
-    print("That username is already taken.")
-    return
+  username = input("Creat Username: ")
+  password = input("Create Password: ")
   salt = random.randint(1000, 9999)
   newPassword = f"{password}{salt}"
   newPassword = hash(newPassword)
   db[username] = {"password": newPassword, "salt": salt}
   print()
-  print("Details Stored")
+  print("User Stored")
   time.sleep(1.7)
   os.system("clear")
-  
+  login()
+
 def login():
   time.sleep(1)
   os.system("clear")
@@ -25,7 +22,7 @@ def login():
   keys = db.keys()
   if username not in keys:
     print()
-    print("That username does not exist")
+    print("Incorrect Information")
     time.sleep(1.5)
     os.system("clear")
     return
@@ -34,13 +31,12 @@ def login():
   newPassword = hash(newPassword)
   if db[username]["password"]==newPassword:
     print("Logged in")
-    time.sleep(1.7)
-    os.system("clear")
     print()
     mainMenu()
 
-
 def mainMenu():
+  time.sleep(1)
+  os.system("clear")
   menu = input("1: Add an entry\n2: View entries\n> ")
   if menu == "1":
     add()
@@ -54,6 +50,7 @@ def add():
   db[key] = thought
   time.sleep(1)
   os.system("clear")
+  mainMenu()
 
 def view():
   matches = db.prefix("mes")
@@ -65,22 +62,26 @@ def view():
     time.sleep(0.3)
     counter+=1
     if(counter%1==0):
-      carryOn = input("See next entry? (yes or no):\n> ")
+      carryOn = input("Next entry? (yes or no):\n> ")
       print()
       if(carryOn.lower()=="no"):
         break
   time.sleep(1)
   os.system("clear")
+  mainMenu()
+
 
 while True:
+  keys = db.keys()
+  for key in keys:
+    del db[key]
   print("Day 72: Secret Diary")
   print()
   print("Login\n")
-  for keys in db.keys():
-    for key in keys:
-      if keys < 1:
-        newUser()
-        time.sleep(1)
-        os.system("clear")
-  else:
-      login()
+  keys = db.keys()
+  if len(keys)<1:
+    newUser()
+    time.sleep(1)
+    os.system("clear")
+  elif len(keys)>0:
+    login()
