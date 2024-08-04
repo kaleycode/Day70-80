6ee7be912329f3943374f03398bf5ce8a60c7659
kaleycode <readingkale@gmail.com>
2024-08-04 12:03:17 -0700

    72notes.py

--- 72notes.py
+++ 72notes.py
@@ -1,3 +1,5 @@
+#first attempt from day 72
+
 from replit import db
 import os, time, datetime, random
 def newUser():
@@ -74,16 +76,62 @@ while True:
   print()
   for key in db.keys():
     if key <= 0:
-      newUser()
-  print("1: Create User\n2: Login\n")
+       newUser()
+    else:
+      login()
+    # I messed up above -- it should check the length like this to see if it's the first time:
+      #keys = db.keys()
+    # if len(keys)<1:
+
+
+
+#Here is the solution from replit: 
+
+from replit import db
+import datetime, os, time, random
+def addEntry():
+  time.sleep(1)
+  os.system("clear")
+  timestamp = datetime.datetime.now()
+  print(f"Diary entry for {timestamp}")
   print()
-  choice = input("> ")
-  if choice == "1":
-    newUser()
-  elif choice == "2":
-    login()
+  entry = input("> ")
+  db[timestamp] = entry
+def viewEntry():
+  keys = db.prefix("2")
+  for key in keys:
+    time.sleep(1)
+    os.system("clear")
+    print(f"""{key}
+    {db[key]}""")
+    print()
+    opt = input("Next or exit? > ")
+    if(opt.lower()[0]=="e"):
+      break
+keys = db.keys()
+if len(keys)<1: #checking to see if it's the first time
+  print("First Run > Create account")
+  username = input("Username > ")
+  password = input("Password > ")
+  salt = random.randint(0,9999999)
+  newPassword = hash(f"{password}{salt}")
+  db[username] = {"password": newPassword, "salt": salt}
+else:
+  print("Log in")
+  username = input("Username > ")
+  password = input("Password > ")
+  if username not in keys:
+    print("Username or password incorrect")
+    exit()
+  salt = db[username]["salt"]
+  newPassword = hash(f"{password}{salt}")
+  if db[username]["password"]!=newPassword:
+    print("Username or password incorrect")
+    exit()
+while True:
+  os.system("clear")
+  menu = input("1: Add\n2: View\n> ")
+  if menu == "1":
+    addEntry()
   else:
-    keys = db.keys()
-    for key in keys:
-      print(db[key])
-      print()
+    viewEntry()
